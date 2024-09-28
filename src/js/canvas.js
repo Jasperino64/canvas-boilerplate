13f7754664831d1b4f97c711ddede7d3582f6db8
Jasperino64 <jasperschen@gmail.com>
2024-09-28 08:20:01 -0400

    Circular

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -27,46 +27,73 @@ addEventListener('resize', () => {
 })
 
 // Objects
-class Object {
+const velocity = 0.05
+class Particle {
   constructor(x, y, radius, color) {
     this.x = x
     this.y = y
+    this.originalX = x
+    this.originalY = y
     this.radius = radius
     this.color = color
+    this.radians = Math.random() * Math.PI * 2
+    this.distanceFromCenter = utils.randomIntFromRange(100, 200)
+    this.lastMouse = { x: x, y: y }
+
   }
 
-  draw() {
+  draw(lastPoint) {
     c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
+    c.strokeStyle = this.color
+    c.moveTo(lastPoint.x, lastPoint.y)
+    c.lineTo(this.x, this.y)
+    c.lineWidth = this.radius
+    c.stroke()
+    // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    // c.fillStyle = this.color
+    // c.fill()
     c.closePath()
   }
 
   update() {
-    this.draw()
+    // Move points over time
+    const lastPoint = { x: this.x, y: this.y }
+    // Drag effect
+    this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05
+    this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05
+    this.radians += velocity
+    this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter
+    this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter
+    this.draw(lastPoint)
   }
 }
 
 // Implementation
-let objects
+let particles
 function init() {
-  objects = []
+  particles = []
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
+  for (let i = 0; i < 100; i++) {
+    let i = Math.random() * Math.PI * 2
+    let x = canvas.width / 2
+    let y = canvas.height / 2
+    let radius = Math.random() * 4 + 1
+     let color = utils.randomColor(colors)
+    particles.push(new Particle(x, y, radius, color))
   }
 }
 
 // Animation Loop
 function animate() {
   requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+  // c.clearRect(0, 0, canvas.width, canvas.height)
+  c.fillStyle = 'rgba(255, 255, 255, 0.05)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  c.shadowBlur = 0.5
+  particles.forEach(p => {
+   p.update()
+  })
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
 }
 
 init()
